06eba6bb080e5201834811774541eec37bd1d013
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-11-25 16:38:32 +0200

    Create E. Lowest Number.cpp

---
+++ Array Sheet/E. Lowest Number.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<cstdio>
+#include<string>
+#include <map>
+#include<numeric>
+#define  ll long long
+using namespace std;
+//#define sc(x,y) scanf_s("%d %d", &x,&y)
+//int LinearSearch(vector<ll>v, int n, ll x)
+//{
+//   
+//    for (int i = 0; i < n; i++) {
+//        if (v[i] == x)
+//            return i;
+//    }
+//    return -1;
+//}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+   
+    int size;
+    int input;
+    vector<int>v;
+    cin >> size;
+    while (size--) {
+        cin >> input;
+        v.push_back(input);
+    } 
+    int min = *min_element(v.begin(), v.end());
+    int minIndex = distance(v.begin(), min_element(v.begin(), v.end())) + 1;
+    cout << min << " " <<minIndex;
+   
+    return 0;
+}
