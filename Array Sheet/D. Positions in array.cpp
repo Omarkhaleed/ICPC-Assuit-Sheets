6891e4b0dba79aa29a6152e45349a8d0e9448935
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-11-25 16:37:42 +0200

    Create D. Positions in array.cpp

---
+++ Array Sheet/D. Positions in array.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<cstdio>
+#include<string>
+#include <map>
+#include<numeric>
+#define  ll long long
+using namespace std;
+//#define sc(x,y) scanf_s("%d %d", &x,&y)
+//int LinearSearch(vector<ll>v, int n, ll x)
+//{
+//   
+//    for (int i = 0; i < n; i++) {
+//        if (v[i] == x)
+//            return i;
+//    }
+//    return -1;
+//}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+   
+    int size;
+    int input;
+    vector<int>v;
+    cin >> size;
+    while (size--) {
+        cin >> input;
+        v.push_back(input);
+    } 
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] <= 10) {
+            cout << "A[" << i << "] = " << v[i] << "\n";
+       }
+    }
+   
+    return 0;
+}
