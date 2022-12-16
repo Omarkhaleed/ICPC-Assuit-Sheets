fbf327ddabeb870ce77107dcbe119cd026248648
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-12-16 21:41:47 +0200

    Create B. Watermelon.cpp

---
+++ General Easy Sheet/B. Watermelon.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<cstdio>
+#include<string>
+using namespace std;
+//#define sc(x,y) scanf_s("%d %d", &x,&y)
+int main()
+{
+   
+   
+    int weight;
+    cin >> weight;
+    if (weight % 2 == 0 && weight>2)
+        cout << "YES" << "\n";
+    else
+        cout << "NO" << "\n";
+      
+        
+   
+   
+  //system("Pause");
+    return 0;
+}
+
+ 
