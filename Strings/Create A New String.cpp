171a53b18dbe54fb5561d9415e9d25398cb24413
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-12-16 21:02:49 +0200

    Add files via upload

---
+++ Strings/Create A New String.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<cmath>
+#include<string.h>
+#include <cstring>
+#include<algorithm>
+#include<cstdio>
+#include<string>
+#include <map>
+#include<numeric>
+#include <set>
+#define  ll long long
+using namespace std;
+//#define sc(x,y) scanf_s("%d %d", &x,&y)
+
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	/*for (int i = 0; i <= 5; i++) {*/
+	
+	string s1, s2;
+	cin >> s1 >> s2;
+	cout << s1.size() << " " << s2.size() << "\n";
+	cout << s1 << " " << s2 << "\n";
+
+	//}
+
+	return 0;
+}
